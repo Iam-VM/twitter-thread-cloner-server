d1bf07f0a00ee154782ef2cb9c987dd7e4f8ccfa
Iam-VM <vishnumohanantheking@gmail.com>
2021-02-03 23:28:16 +0530

    solved video tag issue

--- twitterScript/create_zip.py
+++ twitterScript/create_zip.py
@@ -56,7 +56,7 @@ def extract_media(tweet_response, dir_name):
                     sys.stdout.write("EMN:{}".format(count))
                     sys.stdout.flush()
                     if media["type"] == "photo":
-                        wget.download(media["media_url"], "{}/{}".format(dir_name, media["media_url"].split("/")[-1]), bar=None)
+                        wget.download(media["media_url"], "{}/{}".format(dir_name, media["media_url"].split("/")[-1].split("?")[0]), bar=None)
                         count += 1
                     elif media["type"] == "video":
                         if "video_info" in media:
@@ -64,13 +64,13 @@ def extract_media(tweet_response, dir_name):
                                 for video in media["video_info"]["variants"]:
                                     if "content_type" in video:
                                         if video["content_type"] == "video/mp4":
-                                            wget.download(video["url"], "{}/{}".format(dir_name, video["url"].split("/")[-1]), bar=None)
+                                            wget.download(video["url"], "{}/{}".format(dir_name, video["url"].split("/")[-1].split("?")[0]), bar=None)
                                             count += 1
                                             downloaded = True
                                             break
                         if not downloaded:
                             wget.download(media["media_url"],
-                                          "{}/{}".format(dir_name, media["media_url"].split("/")[-1]), bar=None)
+                                          "{}/{}".format(dir_name, media["media_url"].split("/")[-1].split("?")[0]), bar=None)
                             count += 1
                     elif media["type"] == "animated_gif":
                         if "video_info" in media:
@@ -78,16 +78,16 @@ def extract_media(tweet_response, dir_name):
                                 for video in media["video_info"]["variants"]:
                                     if "content_type" in video:
                                         if video["content_type"] == "video/mp4":
-                                            wget.download(video["url"], "{}/{}".format(dir_name, video["url"].split("/")[-1]), bar=None)
+                                            wget.download(video["url"], "{}/{}".format(dir_name, video["url"].split("/")[-1].split("?")[0]), bar=None)
                                             count += 1
                                             downloaded = True
                                             break
                         if not downloaded:
-                            wget.download(media["media_url"], "{}/{}".format(dir_name, media["media_url"].split("/")[-1]), bar=None)
+                            wget.download(media["media_url"], "{}/{}".format(dir_name, media["media_url"].split("/")[-1].split("?")[0]), bar=None)
                             count += 1
                     else:
 
-                        wget.download(media["media_url"], "{}/{}".format(dir_name, media["media_url"].split("/")[-1]), bar=None)
+                        wget.download(media["media_url"], "{}/{}".format(dir_name, media["media_url"].split("/")[-1].split("?")[0]), bar=None)
                         count += 1
 
 
